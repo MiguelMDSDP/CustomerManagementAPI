7dadcf169fc66c7b0601ef6e7d52bfca06ef4e2b
Miguel dos Santos <miguelmartinsdossantos@gmail.com>
2023-09-08 13:56:20 -0300

    :bug: Add username to login response

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,8 +3,6 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using CustomerManagementAPI.Models;
 
 namespace CustomerManagementAPI.Controllers
@@ -49,6 +47,7 @@ namespace CustomerManagementAPI.Controllers
 
         return Ok(new
         {
+          username = model.Username,
           token = new JwtSecurityTokenHandler().WriteToken(token)
         });
       }
